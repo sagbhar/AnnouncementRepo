c6e1d4e263b4ab7ec90e5f62dee477ae89caeda2
Sagnik <sagbikbhar@gmail.com>
2021-02-18 22:36:42 +0530

    Second Commit

---
+++ AnouncementService/src/main/java/com/sagnik/demo/AnouncementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sagnik.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnouncementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnouncementServiceApplication.class, args);
+	}
+
+}
---
+++ AnouncementService/src/main/java/com/sagnik/demo/model/Announcement.java
@@ -0,0 +1,38 @@
+package com.sagnik.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Announcement")
+public class Announcement {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String title;
+	private String description;
+	private String date;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
